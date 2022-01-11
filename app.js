c743c21e8394809cad8772b43c6e897917037f17
Aaryan <Aaryanarora1871@gmail.com>
2022-01-11 15:22:41 +0530

    single server

--- app.js
+++ app.js
@@ -1,12 +1,31 @@
 // Node server
-
-const io = require('socket.io')(https://loneychat.herokuapp.com/, {
+var express = require("express");
+var app = express();
+var http = require('http').Server(app); 
+// var fs = require('fs'); //require filesystem module
+var io = require('socket.io')(http, {
     cors: {
       origin: '*',
     }
   });
+  app.use(express.static('public'));
+  app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html');
+ });  
 const users = {};
+http.listen(8080); //listen to port 8080
 
+// function handler (req, res) { //create server
+//     fs.readFile(__dirname + '/index.html', function(err, data) { //read file index.html in public folder
+//       if (err) {
+//         res.writeHead(404, {'Content-Type': 'text/html'}); //display 404 on error
+//         return res.end("404 Not Found");
+//       }
+//       res.writeHead(200, {'Content-Type': 'text/html'}); //write HTML
+//       res.write(data); //write data from index.html
+//       return res.end();
+//     });
+//   }
 io.on('connection',socket=>{
 
     socket.on('new-user-joined', name=>{ //socket.on used for various things to happen in client side and all the events name like 'new-user-joined' are custom
